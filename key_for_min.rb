0ee0b1bc4a241964e8074c2605c9f8fdb7e3bc96
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 03:10:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
+ smallest_value=0
   name_hash.each do |name, price|
-    if name == name.start_with("b")
+    if price>smallest_value
+      smallest_value=price
       name
-end
-end
