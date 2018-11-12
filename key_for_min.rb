1d670dfb08eb5279161db06f747dc04c2b9bebcf
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 03:16:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
  smallest_value=9000
- smallest_name = nil
+ smallest_name = nul
   name_hash.each do |name, price|
     if price<smallest_value
       smallest_value=price
